a686a3ece3df660b531025a6aa3def6b4b1421ea
sunilkatwal9 <sunilkatwal9@gmail.com>
2021-06-03 03:46:37 +0000

    Added gitignore

--- personal_porfolio/settings.py
+++ personal_porfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uixtuup5_gvqdvt$cri0s-xv^u9(0m9x!%0is147#v34eod8j^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sunilkatwal.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =  BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
